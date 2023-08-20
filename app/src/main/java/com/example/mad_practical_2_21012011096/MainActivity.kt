31fb40aefd9f5d82adfae5c29b8846cf7887e718
Parth2517 <patelparth88495812@gmail.com>
2023-08-20 23:22:36 +0530

    hello

--- app/src/main/java/com/example/mad_practical_2_21012011096/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_2_21012011096/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         showMessage("onCreate() method is called")
     }
+
     override fun onStart() {
         super.onStart()
         showMessage("onStart() method is called")
@@ -21,8 +24,34 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         showMessage("OnResume() method is called")
     }
-    fun showMessage(msg:String){
+
+    override fun onPause() {
+        super.onPause()
+        showMessage("onPause() Method called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMessage("onStop() method is called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showMessage("onRestart() is called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showMessage("onDestroy() Method is called")
+    }
+
+    fun showMessage(msg: String) {
         Log.i(TAG, "$msg")
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        var v = findViewById<ConstraintLayout?>(R.id.Parth96)
+        if (v != null) {
+            Snackbar.make(v, msg, Snackbar.LENGTH_SHORT).show()
+
+        }
     }
 }
